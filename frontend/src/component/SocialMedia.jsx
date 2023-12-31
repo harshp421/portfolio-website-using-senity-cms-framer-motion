6921b0fa984101890e62888732ff94ab2460bcbc
harsh <harshparmar0421@gmail.com>
2023-12-31 11:16:38 +0530

    resume and social media link added

--- frontend/src/component/SocialMedia.jsx
+++ frontend/src/component/SocialMedia.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import {BsTwitter,BsInstagram} from 'react-icons/bs'
-import {FaFacebook} from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
-   <div className="app__social">
-    <div>
-        <BsTwitter/>
-    </div>
-    <div>
-        <BsInstagram/>
-    </div>
-    <div>
-        <FaFacebook/>
-    </div>
-   </div>
-  )
-}
+    <div className="app__social">
+      <a href="https://github.com/harshp421" target="_blank">
+        <div>
+          <FaGithub />
+        </div>
+      </a>
 
-export default SocialMedia
+      <a href="https://www.linkedin.com/in/harsh-parmar-717834232/" target="_blank">
+        <div>
+          <FaLinkedin />
+        </div>
+      </a>
+
+      <a href="https://www.instagram.com/harsh_js/" target="_blank">
+        <div>
+          <BsInstagram />
+        </div>
+      </a>
+
+      <a href="http://" target="_blank">
+        <div>
+          <FaFacebook />
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default SocialMedia;
--- frontend/src/conainer/Header/Header.jsx
+++ frontend/src/conainer/Header/Header.jsx
@@ -1,76 +1,70 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import "./Header.scss"
-import { images } from '../../constant'
-import AppWrap from '../../Wrapper/AppWrap'
-const scalevarient={
-  whileInView:{
-    scale:[0,1],
-    opacity:[0,1],
-    transition:{
-      duration:2,
-      ease:'easeInOut'
-    }
-  }
-}
+import React from "react";
+import { motion } from "framer-motion";
+import "./Header.scss";
+import { images } from "../../constant";
+import AppWrap from "../../Wrapper/AppWrap";
+const scalevarient = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <div  className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
-      whileInView={{x:[-100,0],opacity:[0,1]}}
-      transition={{duration:2}}
-      className="app__header-info"
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
+        className="app__header-info"
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
             <span> 👋</span>
-            <div style={{marginLeft:20}}>
+            <div style={{ marginLeft: 20 }}>
               <p className="p-text">HELLO ,I AM</p>
-                 <h1 className="head-text">
-                  Harsh
-                 </h1>
+              <h1 className="head-text">Harsh</h1>
             </div>
           </div>
           <div className="tag-cmp app__flex">
-          <p className="p-text">Web Devloper</p>
-          <p className="p-text">Freelancer</p>
+            <p className="p-text">Web Devloper</p>
+            <p className="p-text">Freelancer</p>
           </div>
         </div>
-
       </motion.div>
 
-      
       <motion.div
-       whileInView={{x:[-100,0],opacity:[0,1]}}
-       transition={{duration:0.5,delayChildren:0.5}}
-       className="app__header-img">
-     <img src={images.profile} alt="" srcset="" />
-    <motion.img
-     whileInView={{scale:[0,1]}}
-     transition={{duration:1,ease:'easeInOut'}}
-     
-     src={images.circle}
-     alt="profilr_circle"
-     className="overlay_circle"
-    />
-     </motion.div>
-      
-      
-      <motion.div
-      variants={scalevarient}
-      whileInView={scalevarient.whileInView}
-      className="app__header-circles"
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
       >
-        {[images.node,images.react,images.sass].map((circle,index)=>
-        (
-         <div className='circle-cmp app__flex' key={`circle-${index}`}> 
-         <img src={circle} alt="circle" />
-         </div> 
+        <img src={images.profile} alt="profile-image" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profilr_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scalevarient}
+        whileInView={scalevarient.whileInView}
+        className="app__header-circles"
+      >
+        {[images.node, images.react, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
         ))}
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default AppWrap(Header,'home');
+export default AppWrap(Header, "home");
